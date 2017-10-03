aa674b2ec8892d8162844afce376753698ccc86d
Mehdhi <mehdhi@cinergix.com>
2017-10-03 13:13:46 +0530

    updated constants

--- src/constants.ts
+++ src/constants.ts
@@ -3,3 +3,5 @@ export const MARKER_CUSTOM_CODE_END: string = '// ts-mockgen:custom-code-end';
 
 export const REGEX_MARKER_CUSTOM_CODE_BEGIN: RegExp = /(\/\/ ts-mockgen:custom-code-begin)/g;
 export const REGEX_MARKER_CUSTOM_CODE_END: RegExp = /(\/\/ ts-mockgen:custom-code-end)/g;
+
+export const TAB = '    ';
