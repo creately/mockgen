c0327026ccffff23c7ff999fc829a4b926b13a80
Mehdhi <mehdhi@cinergix.com>
2017-10-03 11:57:14 +0530

    Added class info

--- src/types/class-info.ts
+++ src/types/class-info.ts
@@ -3,7 +3,33 @@ import { ClassMember } from './class-member';
 export class ClassInfo {
     // isAbstract();
 
-    imports: any;
-    members: ClassMember[];
-
+    private _imports: { [key: string]: string }[];
+    private _members: ClassMember[];
+    private _name: string;
+    private _typeParameters: string[];
+    private _userWrittenCodes: string;
+    private _parentClassName: string;
+    private _parentClassPath: string;
+    private _classPath: string;
+    
+    public get imports() : { [key: string]: string }[] {
+        return this._imports;
+    }
+    
+    public get members() {
+        return this._members;
+    }
+    
+    public get name() {
+        return this._name;
+    }
+    
+    public get typeParameters() {
+        return this._typeParameters;
+    }
+    
+    public get userWrittenCodes() {
+        return this._userWrittenCodes;
+    }
+    
 }
